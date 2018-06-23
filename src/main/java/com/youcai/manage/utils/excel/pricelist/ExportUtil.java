7f4b5ece1c774ba8ff4c5feb33b1255fcdb62e93
one <1729711680@qq.com>
2018-06-23 08:49:05 +0800

    '-'

--- src/main/java/com/youcai/manage/utils/excel/pricelist/ExportUtil.java
+++ src/main/java/com/youcai/manage/utils/excel/pricelist/ExportUtil.java
@@ -5,6 +5,8 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
 
 import javax.imageio.ImageIO;
@@ -17,7 +19,7 @@ import java.util.Arrays;
 public class ExportUtil {
 
     /*------------ 文件 -------------*/
-    public static final String LOGO_IMAGE_NAME = "classpath:img/logo.png";
+    public static final String LOGO_IMAGE_NAME = "static/img/logo.png";
 
     /*------------ 单元格 -------------*/
     public static final short CELL_HEIGHT_SM = (short)0x140;
@@ -109,7 +111,8 @@ public class ExportUtil {
         Workbook workbook = sheet.getWorkbook();
         BufferedImage bufferImg = null;
         ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
-        bufferImg = ImageIO.read(ResourceUtils.getFile(img));
+        ClassPathResource classPathResource = new ClassPathResource(img);
+        bufferImg = ImageIO.read(classPathResource.getInputStream());
         ImageIO.write(bufferImg, suffix, byteArrayOut);
         HSSFPatriarch patriarch = sheet.createDrawingPatriarch();
         patriarch.createPicture(anchor, workbook.addPicture(byteArrayOut.toByteArray(), ExportUtil.pictureType(suffix)));
